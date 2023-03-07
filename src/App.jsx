e96a30decebd45fbd5d471a1b5272ee84c2031a0
Eduardo <el_alienx@hotmail.com>
2023-03-07 16:47:36 +0100

    adding pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+// Node modules
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Project files
+import Login from "./pages/Login";
+import RecoverPassword from "./pages/RecoverPassword";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/recover-password" element={<RecoverPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/pages/RecoverPassword.jsx
@@ -0,0 +1,7 @@
+export default function RecoverPassword() {
+  return (
+    <div>
+      <h1>Recover Password Page</h1>
+    </div>
+  );
+}
